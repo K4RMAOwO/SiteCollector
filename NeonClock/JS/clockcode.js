b923fd4b1cf726ede9d178c56fedb671a65c4ffc
K4RMAOwO <leviackerman506@gmail.com>
2023-11-20 13:07:37 +0100

    Create a digital clock html and javarcript are concluded, css need to be finished

---
+++ NeonClock/JS/clockcode.js
@@ -0,0 +1,28 @@
+let hours = document.getElementById('hours');
+let minutes = document.getElementById('minutes');
+let seconds = document.getElementById('seconds');
+let ampm = document.getElementById('ampm');
+
+setInterval(() =>{
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+
+    var am = h >= 12 ? 'PM' : 'AM';
+
+    //Convert 24Hrs clock to 12 Hrs clock
+
+    if(h > 12){
+        h = h - 12;
+    }
+
+    //Add 0 before single digit number
+    h = (h < 10) ? "0" + h : h
+    m = (m < 10) ? "0" + m : m
+    s = (s < 10) ? "0" + s : s
+
+    hours.innerHTML = h;
+    minutes.innerHTML = m;
+    seconds.innerHTML = s;
+    ampm.innerHTML = am;
+})
